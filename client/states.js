9a3a2f06d1fc8c995ba50bd76e84407a60cfde50
Francisco Aviles <francisco.aviles@flex.com>
2017-09-12 09:49:59 -0700

    [Studio] Add screen "test"

--- client/states.js
+++ client/states.js
@@ -33,5 +33,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.test', {
+        views: {
+            app: {
+                controller: 'app_test',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.test');
+                }
+            }
+        }
     });
 });
