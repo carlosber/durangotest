b03abca3e3911313259a697deb85994ba95c1c01
Francisco Aviles <francisco.aviles@flex.com>
2017-09-12 14:04:26 -0700

    [Studio] Add screen "SpiderChart"

---
+++ client/controllers/app/spiderchart.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_spiderchart', app_spiderchart);
+function app_spiderchart($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.spiderchart', {
+        views: {
+            app: {
+                controller: 'app_spiderchart',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.spiderchart');
+                }
+            }
+        }
     });
 });
