9110a169ee17409f00bdab8cdbb9030299f78279
Francisco Aviles <francisco.aviles@flex.com>
2017-09-12 14:14:11 -0700

    [Studio] Modified screen "SpiderChart"

--- client/controllers/app/spiderchart.js
+++ client/controllers/app/spiderchart.js
@@ -2,4 +2,8 @@ angular.module('app').controller('app_spiderchart', app_spiderchart);
 function app_spiderchart($scope, app) {
     'use strict';
     app.init($scope);
+    
+    function onSwipe(algo){
+        alert("test "+ algo);
+    }
 }
